8463b41f0d2b38e54e07553f8e333402627eb1e6
Xhulio Balli <hasanbisha82@gmail.com>
2019-06-21 23:06:55 +0200

    Added logout option and fixed security issues

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const axios = require("axios");
 const qs = require("querystring");
 const mongoose = require("mongoose");
-const exphbs = require('express-handlebars');
+const exphbs = require("express-handlebars");
+const session = require("express-session");
 
 const app = express();
 
@@ -29,32 +30,39 @@ let repoSchema = mongoose.Schema({
   owner: String
 });
 
-// Register Handlebars view engine
-app.engine('handlebars', exphbs());
-// Use Handlebars view engine
-app.set('view engine', 'handlebars');
-
 let Repository = mongoose.model("Repository", repoSchema);
 //Model of Repositories
+
+// Midleware
+// Register Handlebars view engine
+app.engine("handlebars", exphbs());
+// Use Handlebars view engine
+app.set("view engine", "handlebars");
+// Express sessions midleware
+app.use(session({ secret: "ssshhhhh" }));
+// Midleware
 
 //Tvendosme ktu ngaq esht sa per praktik
 let client_id = "2237242856abff508eea";
 let redirect_uri = "http://localhost:5000/user/signin/callback";
 let client_secret = "c62a58231e26409023dd994acde0f50bfd874a31";
-let access_token = "";
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get("/", (req, res) => {
+  res.render("index", { logout: "Yep" });
 });
 
 app.get("/login", (req, res) => {
-  const GitHubUrl =
-    "https://github.com/login/oauth/authorize?" +
-    qs.stringify({
-      client_id: client_id,
-      redirect_uri: redirect_uri
-    });
-  res.redirect(GitHubUrl);
+  if (req.session.username) {
+    res.redirect("/user");
+  } else {
+    const GitHubUrl =
+      "https://github.com/login/oauth/authorize?" +
+      qs.stringify({
+        client_id: client_id,
+        redirect_uri: redirect_uri
+      });
+    res.redirect(GitHubUrl);
+  }
 });
 
 app.all("/user/signin/callback", (req, res) => {
@@ -67,32 +75,56 @@ app.all("/user/signin/callback", (req, res) => {
       code: code
     })
     .then(res => {
-      access_token = res.data.slice(
+      req.session.access_token = res.data.slice(
         res.data.indexOf("n=") + 2,
         res.data.indexOf("&s")
       );
     });
 
-  //res.redirect
-  res.redirect('/user');
+  setTimeout(() => {
+    //res.redirect
+    res.redirect("/user");
+  }, 1000);
 });
 
-app.get('/user', (req, res) => {
-  res.render('user');
+// KY ESHT LEMSH
+// Kontrollon nese useri ka access token -> nqs e ka kontrollon nqs ka username -> po pat i faqa vazhdon sic duhet -> po spat e merr -> po spat as access token i ben redirect per ne '/'
+// Tek ky rast specifik kontrollon nqs sapo i ka ber login ne menyr q t evitoj marrjen e username cdo her q vizitohet kjo url
+app.get("/user", (req, res) => {
+  if (req.session.access_token) {
+    if (req.session.username) {
+      // Ktu kemi kur cdo gj esht OKE
+      res.render("user", { username: req.session.username });
+    } else {
+      axios({
+        method: "GET",
+        url: "https://api.github.com/user",
+        headers: {
+          Authorization: `token ${req.session.access_token}`
+        }
+      }).then(response => {
+        setTimeout(() => {
+          req.session.username = response.data.login;
+          res.render("user", { username: req.session.username });
+        }, 1000);
+      });
+    }
+  } else {
+    res.redirect("/");
+  }
 });
 
 app.get("/user/basic_credencials", (req, res) => {
-  axios({
-    method: "GET",
-    url: "https://api.github.com/user",
-    headers: {
-      Authorization: `token ${access_token}`
-    }
-  }).then(response => {
-    User.find({ name: response.data.login }, (err, res) => {
-      if (res.length > 0) {
-        console.log("User alredy exist");
-      } else {
+  if (req.session.username) {
+    // Ktu kemi kur cdo gj esht OKE
+    axios({
+      method: "GET",
+      url: "https://api.github.com/user",
+      headers: {
+        Authorization: `token ${req.session.access_token}`
+      }
+    }).then(response => {
+      User.find({ name: response.data.login }, (err, UserNumber) => {
         //Saving on DB
         const newUser = new User({
           id: response.data.id,
@@ -102,79 +134,80 @@ app.get("/user/basic_credencials", (req, res) => {
           avatar_url: response.data.avatar_url,
           created_at: response.data.created_at
         });
-
-        newUser.save((err, User) => {
-          if (err) {
-            console.log(err);
-          } else {
-            console.log("User added");
-          }
-        });
-      }
-    });
-
-    //   Gona use this if project requires react
-    // res.json([
-    //   {     //   { id: response.data.id },
-    //   { name: response.data.login },
-    //   { email: response.data.email },
-    //   { url: response.data.url },
-    //   { created_at: response.data.created_at } }
-    // ]);
-
-    //Just for handlebars
-    res.render('userDataRequested', 
-    { id: response.data.id,
-      name: response.data.login,
-      email: response.data.email,
-      url: response.data.url,
-      avatar_url: response.data.avatar_url,
-      created_at: response.data.created_at });
-  });
-});
-
-app.get("/user/repo", (req, res) => {
-  axios({
-    method: "GET",
-    url: `https://api.github.com/user/repos?visibility=all`,
-    headers: {
-      Authorization: `token ${access_token}`
-    }
-  }).then(response => {
-    const Urls = response.data.map(repository => {
-      return repository;
-    });
-
-    Urls.forEach(url => {
-      console.log(
-        `${url.id} : ${url.full_name} : ${url.url} : ${url.owner.login}`
-      );
-
-      Repository.find({ url: url.url }, (err, response) => {
-        if (response.length > 0) {
-          console.log("Repository alredy exists");
+        if (UserNumber.length > 0) {
+          // console.log("User alredy exist");
         } else {
-          const newRepo = new Repository({
-            id: url.id,
-            name: url.full_name,
-            url: url.url,
-            owner: url.owner.login
-          });
-
-          newRepo.save((err, response) => {
+          newUser.save((err, User) => {
             if (err) {
               console.log(err);
             } else {
-              console.log("Added the repository");
+              // console.log("User added");
             }
           });
         }
+        //Just for handlebars
+        res.render("userDataRequested", {
+          User: newUser,
+          username: req.session.username
+        });
       });
     });
-    
-    // res.json(Urls);
-    res.render('repoDataRequested', { data: Urls });
+  } else {
+    res.redirect("/");
+  }
+});
+
+app.get("/user/repo", (req, res) => {
+  if (req.session.username) {
+    // Ktu kemi kur cdo gj esht OKE
+    axios({
+      method: "GET",
+      url: `https://api.github.com/user/repos?visibility=all`,
+      headers: {
+        Authorization: `token ${req.session.access_token}`
+      }
+    }).then(response => {
+      const Urls = response.data.map(repository => {
+        return repository;
+      });
+
+      Urls.forEach(url => {
+        Repository.find({ url: url.url }, (err, response) => {
+          if (response.length > 0) {
+            //console.log("Repository alredy exists");
+          } else {
+            const newRepo = new Repository({
+              id: url.id,
+              name: url.full_name,
+              url: url.url,
+              owner: url.owner.login
+            });
+
+            newRepo.save((err, response) => {
+              console.log(err);
+            });
+          }
+        });
+      });
+
+      // res.json(Urls);
+      res.render("repoDataRequested", {
+        data: Urls,
+        username: req.session.username
+      });
+    });
+  } else {
+    res.redirect("/");
+  }
+});
+
+app.get("/logout", (req, res) => {
+  req.session.destroy(err => {
+    if (err) {
+      console.log(err);
+    }
   });
+  res.redirect("/");
 });
 
 app.listen(5000, () => {
